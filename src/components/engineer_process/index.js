7483dcc942e788a0b04cdf33bd11804009fb3f18
wudaown <wudaown@gmail.com>
2020-07-06 23:52:42 +0800

    engineer processing

--- src/components/engineer_process/index.js
+++ src/components/engineer_process/index.js
@@ -1,161 +1,126 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { connect } from "react-redux";
-import { Paper, Grid, Button, Typography, Icon } from "@material-ui/core";
-import SearchBar from "material-ui-search-bar";
+import React, { useEffect, useState } from "react";
+import { Card, Input, Table, Row, Col, Divider, Tooltip, Button } from "antd";
+import { CheckOutlined } from "@ant-design/icons";
+import { GetItemsPipelineAPI, PatchItemsAPI } from "../../api";
+import { openNotification } from "../../utils/commons";
+import { ERROR, INFO, SUCCESS } from "../../utils/constants";
 
-import {
-  updateState,
-  GetInternalWorkOrderItem,
-  PatchInternalWorkOrderItem,
-} from "../../actions/engineer_process_actions";
+const EngineerProcess = () => {
+  const [unprocess, setUnprocess] = useState([]);
+  const [processing, setProcessing] = useState([]);
+  const [search, setSearch] = useState();
 
-const electron = process.env.NODE_ENV === "development" ? null : window.require("electron");
+  const GetSubWorkOrderNum = (match, method) => {
+    const query = JSON.stringify([
+      {
+        $unwind: {
+          path: "$work_order_items",
+          preserveNullAndEmptyArrays: false,
+        },
+      },
+      {
+        $match: match,
+      },
+      {
+        $addFields: {
+          "work_order_items.submit_date": "$submit_date",
+          "work_order_items.internal_deadline": "$internal_deadline",
+        },
+      },
+      {
+        $replaceRoot: {
+          newRoot: "$work_order_items",
+        },
+      },
+    ]);
 
-const useStyle = makeStyles(() => ({
-  root: {
-    margin: "0 auto",
-    marginTop: 10,
-    maxWidth: 600,
-  },
-  paperRoot: {
-    margin: 30,
-  },
-  tableHeader: {
-    paddingTop: 10,
-    paddingBottom: 10,
-    paddingLeft: 10,
-  },
-  tableRow: {
-    paddingLeft: 10,
-  },
-  btn: {
-    marginTop: -12,
-  },
-}));
-
-function EngineerProcess(props) {
-  const classes = useStyle();
-
-  // vars from reducer
-  const { search, data } = props;
-
-  // methods from actions
-  const { updateState, GetInternalWorkOrderItem, PatchInternalWorkOrderItem } = props;
-
-  const renderHeader = () => {
-    if (data) {
-      return (
-        <Grid container justify="space-around" className={classes.tableHeader} spacing={2}>
-          <Grid item xs={2}>
-            <Typography color="primary">工号</Typography>
-          </Grid>
-          <Grid item xs={2}>
-            <Typography color="primary">品名/图号</Typography>
-          </Grid>
-          <Grid item xs={1}>
-            <Typography color="primary">数量</Typography>
-          </Grid>
-          <Grid item xs={1}>
-            <Typography color="primary">单位</Typography>
-          </Grid>
-          <Grid item xs={1}>
-            <Typography color="primary">下单人</Typography>
-          </Grid>
-          <Grid item xs={2}>
-            <Typography color="primary">CAD图档</Typography>
-          </Grid>
-          <Grid item xs={1}>
-            <Typography color="primary">状态</Typography>
-          </Grid>
-          <Grid item xs={2}></Grid>
-        </Grid>
-      );
-    } else {
-      return null;
-    }
+    GetItemsPipelineAPI("work_orders", query)
+      .then(res => {
+        if (res.data === null) {
+          openNotification(INFO, "搜索不到该图号!");
+        } else {
+          res.data.forEach(el => {
+            el.submit_date = el.submit_date.split("T")[0];
+            el.internal_deadline = el.internal_deadline.split("T")[0];
+            if (el.remark === "" || el.remark === undefined) {
+              el.remark = "待处理";
+            }
+          });
+          method(res.data);
+        }
+      })
+      .catch(err => openNotification(ERROR, err));
   };
 
-  const renderData = () => {
-    if (data) {
-      return (
-        <Grid container justify="space-around" spacing={2} className={classes.tableRow}>
-          <Grid item xs={2}>
-            <Typography>{data.item_id}</Typography>
-          </Grid>
-          <Grid item xs={2}>
-            <Typography>{data.item_num}</Typography>
-          </Grid>
-          <Grid item xs={1}>
-            <Typography>{data.qty}</Typography>
-          </Grid>
-          <Grid item xs={1}>
-            <Typography>{data.unit}</Typography>
-          </Grid>
-          <Grid item xs={1}>
-            <Typography>{data.submit_by}</Typography>
-          </Grid>
-          {process.env.NODE_ENV !== "development" && (
-            <Grid item xs={2}>
-              <Button
-                variant="contained"
-                color="primary"
-                className={classes.btn}
-                onClick={() => {
-                  electron.shell.openItem(data.cad_dir);
-                }}
-              >
-                点击打开
-              </Button>
-            </Grid>
-          )}
-          <Grid item xs={1}>
-            <Typography>{data.state}</Typography>
-          </Grid>
-          <Grid item xs={2}>
-            <Button
-              className={classes.btn}
-              variant="contained"
-              color="primary"
-              endIcon={<Icon>send</Icon>}
-              onClick={() => {
-                PatchInternalWorkOrderItem(data.item_id);
-              }}
-            >
-              完成处理
-            </Button>
-          </Grid>
-        </Grid>
-      );
-    } else {
-      return null;
-    }
+  const ProcessSubWorkOrderNum = (value, remark) => {
+    PatchItemsAPI("work_orders", JSON.stringify({ "work_order_items.sub_work_order_num": value.trim() }), {
+      "work_order_items.$.remark": remark,
+    })
+      .then(res => {
+        openNotification(SUCCESS, res);
+        Init();
+      })
+      .catch(err => openNotification(ERROR, err))
+      .finally(() => setSearch(""));
   };
 
+  const Init = () => {
+    GetSubWorkOrderNum({ "work_order_items.remark": { $nin: ["取消工号", "正在处理", "完成处理"] } }, setUnprocess);
+    GetSubWorkOrderNum({ "work_order_items.remark": { $eq: "正在处理" } }, setProcessing);
+  };
+
+  useEffect(() => {
+    Init();
+  }, []);
+
+  const columns = [
+    { title: "下单日期", dataIndex: "submit_date" },
+    { title: "工号", dataIndex: "sub_work_order_num" },
+    { title: "厂内交期", dataIndex: "internal_deadline" },
+    { title: "状态", dataIndex: "remark" },
+  ];
+
+  const processingColumns = [
+    ...columns,
+    {
+      title: "操作",
+      render: data => (
+        <Tooltip title="完成处理">
+          <Button
+            type="link"
+            icon={<CheckOutlined />}
+            onClick={() => {
+              ProcessSubWorkOrderNum(data.sub_work_order_num, "完成处理");
+            }}
+          />
+        </Tooltip>
+      ),
+    },
+  ];
   return (
-    <React.Fragment>
-      <SearchBar
-        className={classes.root}
-        value={search}
-        placeholder="输入工号"
-        onChange={v => updateState("search", v)}
-        onRequestSearch={() => GetInternalWorkOrderItem(search)}
-      />
-      <Paper className={classes.paperRoot}>
-        {renderHeader()}
-        {renderData()}
-      </Paper>
-    </React.Fragment>
+    <Card>
+      <Row gutter={[16, 16]}>
+        <Col span={12}>
+          <Input.Search
+            placeholder="搜索工号"
+            onSearch={value => {
+              ProcessSubWorkOrderNum(value, "正在处理");
+            }}
+            onChange={e => setSearch(e.target.value)}
+            value={search}
+          ></Input.Search>
+        </Col>
+        <Col span={24}>
+          <Divider orientation="left">正在处理</Divider>
+          <Table rowKey="sub_work_order_num" pagination={false} columns={processingColumns} dataSource={processing} />
+        </Col>
+        <Col span={24}>
+          <Divider orientation="left">待处理</Divider>
+          <Table rowKey="sub_work_order_num" pagination={false} columns={columns} dataSource={unprocess} />
+        </Col>
+      </Row>
+    </Card>
   );
-}
-
-const mapStateToProps = ({ EngineerProcessReducer }) => {
-  return {
-    search: EngineerProcessReducer.search,
-    data: EngineerProcessReducer.data,
-  };
 };
 
-export default connect(mapStateToProps, { updateState, GetInternalWorkOrderItem, PatchInternalWorkOrderItem })(
-  EngineerProcess
-);
+export default EngineerProcess;
