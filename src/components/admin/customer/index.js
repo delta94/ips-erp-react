8ae792943cb40dba541fd9fd11ecbb8a194b5031
wudaown <wudaown@gmail.com>
2020-07-08 23:04:48 +0800

    add more fields to customer

--- src/components/admin/customer/index.js
+++ src/components/admin/customer/index.js
@@ -1,6 +1,22 @@
 import React, { useState, useEffect } from "react";
-import { Table, Input, Form, Tooltip, Button, Space, Card, Modal, Row, Col } from "antd";
+import {
+  Table,
+  Input,
+  Form,
+  Tooltip,
+  Button,
+  Space,
+  Card,
+  Modal,
+  Row,
+  Col,
+  Divider,
+  Select,
+  DatePicker,
+  InputNumber,
+} from "antd";
 import { EditOutlined, DeleteOutlined, UndoOutlined, SaveOutlined, SearchOutlined } from "@ant-design/icons";
+import moment from "moment";
 import { GetItemsAPI, InsertItemAPI, RemoveItemAPI } from "../../../api";
 import { openNotification } from "../../../utils/commons";
 import { ERROR, SUCCESS } from "../../../utils/constants";
@@ -38,6 +54,7 @@ const EditableTable = () => {
   const [orgCustomers, setOrgCustomers] = useState([]);
   const [search, setSearch] = useState("");
   const [visible, setVisible] = useState(false);
+  const [country, setCountry] = useState();
 
   const GetCustomers = () => {
     GetItemsAPI("customers")
@@ -98,19 +115,14 @@ const EditableTable = () => {
       editable: true,
     },
     {
-      title: "结算货币",
-      dataIndex: "currency",
-      editable: true,
-    },
-    {
       title: "地址",
       dataIndex: "addr",
       width: "48%",
       editable: true,
     },
     {
-      title: "区域",
-      dataIndex: "area",
+      title: "结算币种",
+      dataIndex: "currency",
       editable: true,
     },
     {
@@ -167,8 +179,8 @@ const EditableTable = () => {
   });
 
   const layout = {
-    labelCol: { span: 8 },
-    wrapperCol: { span: 8 },
+    labelCol: { span: 4 },
+    wrapperCol: { span: 20 },
   };
   const tailLayout = {
     wrapperCol: { offset: 8, span: 8 },
@@ -215,8 +227,16 @@ const EditableTable = () => {
           }}
         />
       </Form>
-      <Modal title="新客户信息" visible={visible} onCancel={() => setVisible(false)} footer={null} destroyOnClose>
+      <Modal
+        title="新客户信息"
+        visible={visible}
+        onCancel={() => setVisible(false)}
+        footer={null}
+        destroyOnClose
+        width="50%"
+      >
         <Form
+          initialValues={{ reconciliation_date: moment() }}
           {...layout}
           name="basic"
           form={newCustomerForm}
@@ -226,25 +246,74 @@ const EditableTable = () => {
               GetCustomers();
               openNotification(SUCCESS, res.message);
               setVisible(false);
+              newCustomerForm.resetFields();
             } else {
               openNotification(ERROR, res.message);
             }
           }}
         >
-          <Form.Item label="客户代号" name="internal" rules={[{ required: true, message: "请输入客户代号" }]}>
+          <Divider orientation="left">基本信息</Divider>
+          <Form.Item label="客户代码" name="internal" rules={[{ required: true, message: "请输入客户代号" }]}>
             <Input />
           </Form.Item>
-
-          <Form.Item label="名称" name="name" rules={[{ required: true, message: "请输入名称" }]}>
+          <Form.Item label="客户名称" name="name" rules={[{ required: true, message: "请输入名称" }]}>
             <Input />
           </Form.Item>
-          <Form.Item label="结算货币" name="currency" rules={[{ required: true, message: "请输入结算货币" }]}>
+          <Form.Item label="详细地址" name="addr" rules={[{ required: true, message: "请输入地址" }]}>
             <Input />
           </Form.Item>
-          <Form.Item label="地址" name="addr" rules={[{ required: true, message: "请输入地址" }]}>
+          <Form.Item label="国家" name="country" rules={[{ required: true, message: "请输入国家" }]}>
+            <Input onChange={e => setCountry(e.target.value === "中国" ? false : true)} />
+          </Form.Item>
+          <Form.Item label="结算币种" name="currency" rules={[{ required: true, message: "请输入结算货币" }]}>
             <Input />
           </Form.Item>
-          <Form.Item label="区域" name="area" rules={[{ required: true, message: "请输入区域" }]}>
+          <Form.Item label="汇率" name="rate" rules={[{ required: true, message: "请输入汇率" }]}>
+            <InputNumber />
+          </Form.Item>
+          <Form.Item label="业务组别" name="bu_group" rules={[{ required: true, message: "请输入业务组别" }]}>
+            <Input />
+          </Form.Item>
+          <Divider orientation="left">收款信息</Divider>
+          <Form.Item label="对账日期" name="reconciliation_date">
+            <DatePicker className="full-width" />
+          </Form.Item>
+          <Form.Item
+            label="收款天数"
+            name="reconciliation_days"
+            rules={[{ required: true, message: "请输入收款天数" }]}
+          >
+            <Select>
+              <Select.Option value={30}>30</Select.Option>
+              <Select.Option value={60}>60</Select.Option>
+              <Select.Option value={90}>90</Select.Option>
+            </Select>
+          </Form.Item>
+          <Form.Item label="是否报关" name="custom" rules={[{ required: true, message: "请选择是否报关" }]}>
+            <Select>
+              <Select.Option value={true}>报关</Select.Option>
+              <Select.Option value={false}>不报关</Select.Option>
+            </Select>
+          </Form.Item>
+          <Form.Item label="是否开票" name="invoice" rules={[{ required: true, message: "请选择是否开票" }]}>
+            <Select>
+              <Select.Option value={true}>开票</Select.Option>
+              <Select.Option value={false}>不开票</Select.Option>
+            </Select>
+          </Form.Item>
+          <Divider orientation="left">运输信息</Divider>
+          <Form.Item
+            label="快递公司"
+            name="shipping_company"
+            rules={[{ required: country, message: "请输入快递公司" }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="快递账号"
+            name="shipping_account"
+            rules={[{ required: country, message: "请输入快递账号" }]}
+          >
             <Input />
           </Form.Item>
           <Form.Item {...tailLayout}>
